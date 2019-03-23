b4d0e664f97a3ba4bd764527f0751cae1f506f6b
TechPrimers <movingtoweb@gmail.com>
2019-03-23 19:20:47 +0530

    added getmapping

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -1,5 +1,6 @@
 package com.techprimers.aws;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping
     public String hello() {
         return "Hello Youtube";
     }
